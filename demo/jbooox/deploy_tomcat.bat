cd8395ca4f0907fa70c439e5c2c75f0366ebfd96
Yong Zhu <yong9981@gmail.com>
2019-02-06 02:01:23 -0700

    2.0.6 Release

--- demo/jbooox/deploy_tomcat.bat
+++ demo/jbooox/deploy_tomcat.bat
@@ -1,21 +1,15 @@
-call mvn clean -DskipTests package 
-
-rd /s/q /q C:\tomcat7\logs
-md C:\tomcat7\logs
-
-rd /s/q /q C:\tomcat7\work
-md C:\tomcat7\work
-
-rd /s/q /q C:\tomcat7\webapps
-md C:\tomcat7\webapps 
-
+call mvn clean -DskipTests package
+ 
+set TomcatFolder=c:\tomcat8
+rd /s/q /q %TomcatFolder%\logs
+md %TomcatFolder%\logs
+rd /s/q /q %TomcatFolder%\work
+md %TomcatFolder%\work
+rd /s/q /q %TomcatFolder%\webapps
+md %TomcatFolder%\webapps 
 cd target
 del ROOT.war
 ren *.war ROOT.war 
-copy ROOT.war C:\tomcat7\webapps\ /y 
-
-c:
-cd C:\tomcat7\bin\
-call startup.bat
-
+copy ROOT.war %TomcatFolder%\webapps\ /y 
+call %TomcatFolder%\bin\startup.bat
 start http://127.0.0.1 
